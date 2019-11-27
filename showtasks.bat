66c6013987eb685c343faf32445c6ac6ae6a3875
kaszakinga90 <kaszakinga90@gmail.com>
2019-11-27 17:53:40 +0100

    Task 21.4

---
+++ showtasks.bat
@@ -0,0 +1,22 @@
+call runcrud.bat
+
+if "%ERRORLEVEL%" == "0" goto browser
+echo.
+echo script has errors - breaking work
+goto fail
+
+:browser
+start chrome.exe "http://localhost:8080/crud/v1/task/getTasks"
+timeout 30
+if "%ERRORLEVEL%" == "0" goto end
+echo.
+echo Browser cannot open.
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
