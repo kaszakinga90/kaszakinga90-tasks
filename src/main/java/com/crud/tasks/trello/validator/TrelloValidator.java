884cc75416e3c168828b7f1e6795f60f1470a01a
kaszakinga90 <kaszakinga90@gmail.com>
2020-02-13 10:19:45 +0100

    Task 32.1

--- src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
+++ src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
@@ -24,11 +24,21 @@ public class TrelloValidator {
     public List<TrelloBoard> validateTrelloBoards(final List<TrelloBoard> trelloBoards) {
 
         LOGGER.info("Starting filtering boards...");
+
+        LOGGER.info("Boards have been filtered. Current list size: " + trelloBoards.size());
+        return trelloBoards;
+    }
+
+    /*
+        public List<TrelloBoard> validateTrelloBoards(final List<TrelloBoard> trelloBoards) {
+
+        LOGGER.info("Starting filtering boards...");
         List<TrelloBoard> filteredBoards = trelloBoards.stream()
                 .filter(trelloBoard -> trelloBoard.getName().equalsIgnoreCase("test"))
                 .collect(Collectors.toList());
         LOGGER.info("Boards have been filtered. Current list size: " + filteredBoards.size());
         return filteredBoards;
     }
+     */
 
 }
